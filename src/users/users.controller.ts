9208bb53b6a9157dbc2ac02a142be664fe2d1e54
drex007 <vykeoj@gmail.com>
2025-12-26 18:07:27 +0100

    feat: added user service to controller as injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,25 @@
 import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly userService: UsersService){
+
+    }
     @Get()
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
-        return []
+        
+        return this.userService.findAll()
     }
 
     @Get(':id')
     findOne(@Param('id') id:string){
-        return {id }
+        return this.userService.findOne(+id)
     }
 
     @Post()
-    create(@Body() user:{ }){
-        return user
+    create(@Body() user:{name:"Ifeany ugba", role:'INTERN'}){
+        return this.userService.create(user)
     }
 
     @Patch(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [
+        {
+            "id":1,
+            "name":"James Aremu",
+            "role": "INTERN"
+        },
+        {
+            "id":2,
+            "name":"Phyna Aremu",
+            "role": "ADMIN"
+        }
+    ]
+
+
+    findAll(role?: ''){
+        if(role){
+            return this.users.filter(user => user.role == role)
+        }
+        return this.users
+    }
+
+
+    findOne(id:number){
+        const user = this.users.find(user => user.id === id)
+        return user
+    }
+
+    create(user:{name:string, role:'INTERN' | 'ADMIN'}){
+        const highestId = [...this.users].sort((a, b) => b.id = a.id)
+        const newUser = {
+            id:highestId[0].id + 1,
+            ...user
+        }
+
+        this.users.push(newUser)
+        return newUser
+
+
+    }
+
+
+    update(id:number, updatedUser:{name:string, role:'INTERN' | 'ADMIN'}){
+        const highestId = this.users
+        const newUser = {
+            id:highestId[0].id + 1,
+            ...updatedUser
+        }
+
+        this.users.push(newUser)
+        return newUser
+
+
+    }
+}
+ 
